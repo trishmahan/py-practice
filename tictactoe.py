33409ab3a4c3f01af894ac0e484e40f48fbfd03f
Trish Mahan <trishmahan@Trishs-MBP.home>
2017-06-24 22:04:33 -0400

    Update tictactoe

--- tictactoe.py
+++ tictactoe.py
@@ -20,15 +20,14 @@ also_no_winner = [[1, 2, 0],
 	[2, 1, 0]]
 
 
-# def DrawBoard(size):
-# 	for i in range(size):	
-# 		print(" ---" * size)
-# 		print("|   " * (size + 1))
-# 	print(" ---" * size)
-# 	return 
-
-def checkWinner(grid):
-	#check diagonals
+def DrawBoard(size):
+	for i in range(size):	
+		print(" ---" * size)
+		print("|   " * (size + 1))
+	print(" ---" * size)
+	return 
+	
+def checkDiagonalsForWinner(grid):
 	if grid[1][1] != 0:
 		diagonalOne = [grid[0][0], grid[1][1], grid[2][2]]
 		if diagonalOne[0] == diagonalOne[1] == diagonalOne[2]:
@@ -36,7 +35,9 @@ def checkWinner(grid):
 		diagonalTwo = [grid[2][0], grid[1][1], grid[0][2]]
 		if diagonalTwo[0] == diagonalTwo[1] == diagonalTwo[2]:
 			return ("The winner is player " + str(diagonalTwo[0]))
-	#check rows/columns
+	else:
+		return False
+def checkRowsColumnsForWinner(grid):
 	for x in range(3):
 		if grid[x][0] != 0
 			row = [grid[x][0], grid[x][1], grid[x][2]]
@@ -47,9 +48,7 @@ def checkWinner(grid):
 			if column[0] == column[1] == column[2]:
 				return ("The winner is player " + str(column[0]))
 	else:
-		return False
+		return False	
 
 
-
-print checkWinner(winner_is_2)
 # DrawBoard(int(input("Please enter the size board you would like (2 - 18):")))
